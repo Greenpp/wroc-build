4ed30600bcf6777967b13910a9d31e6fe23c38b9
Jakub Ciszek <jakubciszek64@gmail.com>
2020-12-04 18:10:43 +0100

    Name, deps, static update

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='WrocBuild',
+    name='wroc-build',
     description='Building footprint segmentation in Wrocław',
     version='0.1.0',
     url='https://github.com/Greenpp/wroc-build',
     author='Jakub Ciszek',
     packages=find_packages(),
-    package_data={'model': ['./seg_model.pt']},
+    package_data={'model': ['./wroclaw_building_footprint/seg_model.pt']},
     install_requires=[
+        'torch',
+        'torchvision',
         'numpy',
         'Pillow',
         'opencv-python',
